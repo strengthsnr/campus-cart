3c92a8102bf259fc0677b5615503b1020d84ed26
strengthsnr <oladejiisrael41@gmail.com>
2026-09-21 08:44:43 +0100

    Set up CampusCart Invenotry

---
+++ main.py
@@ -0,0 +1,30 @@
+"""
+CampusCart - Stage 2
+A simpe command-line shopping application.
+"""
+
+# CampusCart manages products, shopping carts, stock and checkout.
+
+#Creation of inventory dictionary with product details including name, price, and stock quantity.
+inventory = {
+    "101": {
+        "name": "Notebook",
+        "price": 2.50,
+        "stock": 15
+    },
+    "102": {
+        "name": "Pen",
+        "price": 1.50,
+        "stock": 20
+    },
+    "103": {
+        "name": "Pencil",
+        "price": 1.00,
+        "stock": 30
+    },
+    "104": {
+        "name": "Eraser",
+        "price": 0.50,
+        "stock": 25
+    }
+}
